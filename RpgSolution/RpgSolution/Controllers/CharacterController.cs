d95c2ec1b0f70294f9f20918f42be110e501fdc7
emir balcı <emir.balci@demirorenteknoloji.com>
2020-10-07 10:59:28 +0300

    Apiler yazıldı.

---
+++ RpgSolution/RpgSolution/Controllers/CharacterController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RpgSolution.Models;
+
+namespace RpgSolution.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private static Character knight = new Character();
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character
+            {
+                Id = 2,
+                Name = "Emir",
+                Class = RpgClass.Cleric
+            }
+        };
+
+        public IActionResult Get()
+        {
+            return Ok(knight);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(characters);
+        }
+
+        [HttpPost]
+        public IActionResult AddCharacter(Character newCharacter)
+        {
+            characters.Add(newCharacter);
+            return Ok(characters);
+        }
+    }
+}
